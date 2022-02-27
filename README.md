9baf33138e9bdc269f531cda3d83cde0ff47790f
Tettei Shahday-Annang <tetteis@gmail.com>
2022-02-27 22:31:05 +0000

    Finshed set up

---
+++ README.md
@@ -0,0 +1,4 @@
+# React Core Fundamentals
+
+Set up React from scratch with CDNs to have a taste of what it feels like
+to develop in react.
---
+++ index.js
@@ -0,0 +1,24 @@
+ReactDOM.render(
+  <>
+    <nav>
+      <h1>ReactJS</h1>
+      <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+
+    <h1>My awesome website in React</h1>
+
+    <h2>Reasons I ❤️ React</h2>
+
+    <ol>
+      <li>It's composable</li>
+      <li>It's declarative</li>
+      <li>It's a hireable skill</li>
+      <li>It's actively maintained by skilled people</li>
+    </ol>
+  </>,
+  document.getElementById("root")
+);
